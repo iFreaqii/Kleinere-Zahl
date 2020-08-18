4928f75996402a5a65fa4398453baaa337f7c5d7
Nico Bauer <Bauer.Nico1998@outlook.de>
2020-08-18 08:32:29 +0200

    Änderung

---
+++ Kleinere Zahl/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+/// <summary>
+/// Nico Bauer
+/// IS218
+/// AWE
+///  Drei Zahlen werden per Tastatur eingelesen. Bestimme die kleinste der Zahlen und gib sie auf dem Bildschirm aus!
+/// </summary>
+
+namespace Kleinere_Zahl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
